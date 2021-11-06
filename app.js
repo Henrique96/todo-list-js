4e62086070a7ee6d9dd9d74f74c74b8b52e93e0c
Matheus Henrique Pedroso Viana <matheus_pviana@hotmail.com>
2021-11-06 16:41:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+if (window.localStorage.getItem("toDos") == undefined) {
+    let toDos = [];
+    window.localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
+let save = window.localStorage.getItem("toDos");
+let toDos = JSON.parse(save);
+
+addToDoButton.addEventListener('click', function () {
+
+    if (inputField.value != "") {
+        createItem(inputField.value);
+        toDos.push(inputField.value);
+        window.localStorage.setItem("toDos", JSON.stringify(toDos));
+        inputField.value = "";
+    }
+    else if (!inputField.value || inputField.value.trim() == "") {
+        alert("Invalid Input");
+        return;
+    }
+
+});
+
+function createItem(name) {
+
+    let divElement = document.createElement('div');
+    divElement.classList.add('item');
+
+    // text
+
+    let input = document.createElement('input');
+    input.classList.add('paragrafoEstilo');
+    input.setAttribute('style', 'color: grey');
+    input.disabled = true;
+    input.value = name;
+
+    // Remove button
+
+    let button = document.createElement('button');
+    button.classList.add('remove');
+    button.type = 'button';
+    button.innerHTML = '| Excluir';
+    button.addEventListener('click', function () {
+        toDoContainer.removeChild(divElement);
+        let index = toDos.indexOf(name);
+        toDos.splice(index, 1);
+        window.localStorage.setItem("toDos", JSON.stringify(toDos));
+    });
+
+    // Adiciona a div
+
+    toDoContainer.appendChild(divElement);
+    divElement.appendChild(input);
+    divElement.appendChild(button);
+}
+
+for (let i = 0; i < toDos.length; i++) {
+    createItem(toDos[i]);
+}
